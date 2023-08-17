e61f69fafc61f64c70a0b9a83d44cc57ce8c7277
Jorge Teles <jorteles@gmail.com>
2023-08-17 12:50:39 -0300

    Script e carga, import.py. Adicionado o arquivo requirements.txt para instalação do módulo fhirpy. adicionado .gitignore para não enviar ao repositório o ambiente .venv dentro de src arquivo adicionado patients.csv

--- src/import.py
+++ src/import.py
@@ -1,2 +1,44 @@
-import fhirpy
+import asyncio
+from fhirpy import AsyncFHIRClient
+import csv
 
+filepath = '../data/patients.csv'
+client = AsyncFHIRClient(
+        'http://localhost:8080/fhir/',
+        authorization='Bearer TOKEN',
+    )
+
+async def main():
+
+    with open(filepath, encoding='ISO 8859-1') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            #print(row)
+            Nome, Sobrenome = row['Nome'].split(' ', 1)
+            name = [{'use': 'usual' , 'text': row['Nome'] , 'family': Sobrenome, 'given': Nome}] 
+            contactPhone = [{'system':'phone', 'value':row['Telefone'], 'use':'home', 'rank':1}]
+
+            patient = client.resource('Patient', 
+            active=True,
+            identifier=row['CPF'],
+            name=name, 
+            birthdate=row['Data de Nascimento'], 
+            telecom=contactPhone,
+            country=row['País de Nascimento'],
+
+            )
+            await patient.save()
+
+async def get_patient(id):
+    patient = await client.reference('Patient', id).to_resource()
+    #print(patient)
+    return patient
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    patient = (loop.run_until_complete(get_patient(1)))
+    print(patient)
